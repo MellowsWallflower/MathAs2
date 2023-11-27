95f52d3be10f726d1e3bf7462e75e1c0b0b9f060
Weezil <200568840@student.georgianc.on.ca>
2023-11-27 14:20:35 -0500

    Part 2
    Added Part 2

---
+++ MathAs2/Assets/Scripts/Gavins Scripts/GravitationalForce.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitationalForce : MonoBehaviour
+{
+    public float Gravity = 9.8f;
+    public float Mass;
+    float Velocity;
+    float Resistance = 0;
+    public GameObject Ground;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+    private void Fall()
+    {
+        gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - Velocity);
+    }
+
+    // Update is called once per fram
+    void Update()
+    {
+        Ground.transform.position = new Vector2(gameObject.transform.position.x, Ground.transform.position.y);
+        if (gameObject.transform.position.y > Ground.transform.position.y + Ground.transform.localScale.y)
+        {
+            Fall();
+            Velocity = Mathf.Clamp(Resistance, 0, Gravity) * Mass * Time.deltaTime;
+            Resistance += 0.1f;
+
+        }
+        else if (gameObject.transform.position.y <= Ground.transform.position.y + Ground.transform.localScale.y)
+        {
+            Velocity = 0;
+        }
+        
+    }
+}
